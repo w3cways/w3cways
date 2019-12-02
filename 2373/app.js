3028e77724dc6cf193cec8e8de0a3b109cd5e3b0
Tony <6859510+w3cways@users.noreply.github.com>
2019-12-02 13:53:48 +0800

    2373_koa中使用websocket(ws)，并共享端口

--- 2373/app.js
+++ 2373/app.js
@@ -6,12 +6,13 @@ const app = new Koa();
 
 const WebSocketApi = require('./util/ws');//引入封装的ws模块
 
-const server = http.createServer(app.callback());
+const server = http.createServer(app.callback())
 
 const wss = new WebSocket.Server({// 同一个端口监听不同的服务
     server
-})
+});
 
 WebSocketApi(wss)
 
-server.listen(config.port)
+server.listen(3000)
+console.log("服务启动成功，端口号:3002 ")
---
+++ 2373/util/ws.js
@@ -0,0 +1,11 @@
+//util/ws.js
+module.exports = wss => {
+    wss.on('connection', function connection(ws) {
+        ws.on('message', function incoming(message) {
+            console.log('received: %s', message);
+        });
+
+        ws.send('something');
+    });
+
+}
